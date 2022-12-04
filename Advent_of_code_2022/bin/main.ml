371f8f59a4b16a0f0f5bcdfb40a7aab7e13fa552
Yag000 <80483460+Yag000@users.noreply.github.com>
2022-12-04 10:52:10 +0100

    Day4 (#5)
    * Day 4.1
    
    * Day 4.1 working version
    
    * Day 4.2

--- Advent_of_code_2022/bin/main.ml
+++ Advent_of_code_2022/bin/main.ml
@@ -1,3 +1,3 @@
 open Lib
 
-let () = Day3_2.run ()
+let () = Day4_2.run ()
---
+++ Advent_of_code_2022/lib/day4_1.ml
@@ -0,0 +1,24 @@
+let is_contained (x0, x1) (y0, y1) =
+  if x0 >= y0 && x1 <= y1 then true
+  else if y0 >= x0 && y1 <= x1 then true
+  else false
+
+let treat_string s =
+  String.split_on_char ',' s
+  |> List.map (fun x -> String.split_on_char '-' x |> List.map int_of_string)
+  |> List.map (fun x -> (List.nth x 0, List.nth x 1))
+
+let is_string_self_contained s =
+  let l = treat_string s in
+  is_contained (List.nth l 0) (List.nth l 1)
+
+let treat_list =
+  let rec aux acc = function
+    | [] -> acc
+    | x :: xs -> aux (acc + if is_string_self_contained x then 1 else 0) xs
+  in
+  aux 0
+
+let run () =
+  Utilities.read_file "resources/Day4/input.txt"
+  |> treat_list |> Printf.printf "%d"
