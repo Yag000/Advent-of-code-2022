bbd5ab742c582973cd0ed493f46559083f58b500
Yago Iglesias <yago.iglesias-vazquez@etu.u-paris.fr>
2022-12-09 21:55:40 +0100

    add comments to utilities

--- lib/utilities.ml
+++ lib/utilities.ml
@@ -1,3 +1,7 @@
+(* **
+   * read_file takes a filename and reads the contents of the file line by line,
+   * returning a list of strings, where each string represents a line from the file.
+   * *)
 let read_file filename =
   let lines = ref [] in
   let channel = open_in filename in
@@ -10,6 +14,11 @@ let read_file filename =
     close_in channel;
     List.rev !lines
 
+(* **
+   * read_file_reversed takes a filename and reads the contents of the file line by line,
+   * returning a list of strings, where each string represents a line from the file
+   * in reverse order.
+   * *)
 let read_file_reversed filename =
   let lines = ref [] in
   let channel = open_in filename in
@@ -22,6 +31,20 @@ let read_file_reversed filename =
     close_in channel;
     !lines
 
+(* **
+   * string_to_char_list takes a string and returns a list of characters for that string.
+   * *)
 let string_to_char_list s = s |> String.to_seq |> List.of_seq
+
+(* **
+   * head takes a list and returns the first element of the list.
+   * If the list is empty, it raises the "head" exception.
+   * *)
 let head = function [] -> failwith "head" | x :: _ -> x
+
+(* **
+   * tail takes a list and returns a new list that contains all the elements of the
+   * original list except the first one.
+   * If the list is empty, it raises the "tail" exception.
+   * *)
 let tail = function [] -> failwith "tail" | _ :: xs -> xs
