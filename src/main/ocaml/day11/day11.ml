752caadf032780b0c926938876781aeb14416d58
Yago Iglesias <yago.iglesias-vazquez@etu.u-paris.fr>
2022-12-11 11:39:26 +0100

    Ocaml try

---
+++ src/main/ocaml/day11/day11.ml
@@ -0,0 +1,13 @@
+let () =
+  print_newline ();
+  print_endline "----------------- Day 11 -----------------";
+  print_newline ();
+  print_endline "Part 1";
+  print_newline ();
+  Day11_1.run ();
+  print_newline ();
+  print_newline ();
+  print_endline "Part 2";
+  print_newline ()
+(* Day11_2.run ();
+   print_newline ()*)
