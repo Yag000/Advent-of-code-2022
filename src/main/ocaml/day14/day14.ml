01bcdaa8a80123adc9e6b302b1d337faa670578e
Yago Iglesias <yago.iglesias-vazquez@etu.u-paris.fr>
2022-12-13 21:44:29 +0100

    Preparation day 14

---
+++ src/main/ocaml/day14/day14.ml
@@ -0,0 +1,26 @@
+open Lib
+
+let part1 () = ()
+let part2 () = ()
+
+let () =
+  let t1 = Sys.time () in
+  let _ = Utilities.read_file "resources/day13.txt" in
+  print_newline ();
+  print_endline "----------------- Day 12 -----------------";
+  print_newline ();
+  print_endline "Part 1";
+  print_newline ();
+  part1 ();
+  print_newline ();
+  print_newline ();
+  let t2 = Sys.time () in
+  print_endline ("Time: " ^ string_of_float (t2 -. t1) ^ "s");
+  print_newline ();
+  print_endline "Part 2";
+  print_newline ();
+  part2 ();
+  print_newline ();
+  print_newline ();
+  print_endline ("Time: " ^ string_of_float (Sys.time () -. t2) ^ "s");
+  print_newline ()
