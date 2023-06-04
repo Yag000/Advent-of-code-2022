64c48e525583668a5680251812c0c5701c8d9ab4
Yago Iglesias <yago.iglesias-vazquez@etu.u-paris.fr>
2023-06-04 18:37:58 +0200

    More refactoring

--- src/day1.rs
+++ src/day1.rs
@@ -5,63 +5,50 @@ pub fn run() {
 
     let path = "resources/day1.txt";
     let contents = fs::read_to_string(path).expect("Should have been able to read the file");
-    
+
     println!("Part 1");
-    day1_1(&contents);
+    get_sum_max_elements(&contents, 1);
 
     println!("Part 2");
-    day1_2(&contents);
+    get_sum_max_elements(&contents, 3);
 }
 
-fn day1_1 (contents:&str) {
-    let mut max_calories = 0; // I need vectors to do this...
+
+fn get_sum_max_elements(contents: &str, n: u32) {
+    let mut max_calories: Vec<u32> = Vec::new(); // I need vectors to do this...
+    for _ in 0..n{
+        max_calories.push(0);
+    }
     let mut subtotal = 0;
 
     for line in contents.lines() {
         match line.parse::<u32>() {
             Ok(value) => subtotal += value,
             Err(_) => {
-                if subtotal > max_calories {
-                    max_calories = subtotal;
-                }
+                add_to_values(&mut max_calories, subtotal);
                 subtotal = 0
             }
         };
     }
-
-    println!("The max caloriees is: {max_calories}");
-}
-
-
-fn day1_2 (contents: &str) {
-    let mut max_calories = [0; 3]; // I need vectors to do this...
-    let mut subtotal = 0;
-
-    for line in contents.lines() {
-        match line.parse::<u32>() {
-            Ok(value) => subtotal += value,
-            Err(_) => {
-                add_to_max_list(&mut max_calories, subtotal);
-                subtotal = 0
-            }
-        };
+        
+    let mut max = 0;
+    for value in max_calories {
+        max += value;
     }
-
-    let max = max_calories[0] + max_calories[1] + max_calories[2];
-
+        
     println!("The max caloriees is: {max}");
 }
 
-fn add_to_max_list(array: &mut [u32], subtotal: u32) {
-    for (i, value) in array.iter().enumerate() {
+fn add_to_values(values: &mut Vec<u32>, subtotal: u32) {
+    for (i, value) in values.iter().enumerate() {
         if subtotal <= *value {
             continue;
         }
 
-        array[i] = subtotal;
+        values[i] = subtotal;
         // shift the rest of the array
-        for j in i + 1..array.len() {
-            array[j] = array[j - 1];
+        for j in i + 1..values.len() {
+            values[j] = values[j - 1];
         }
         break;
     }
