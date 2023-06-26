e5c18ca088fe345d8b9ff12f60e220c3e7dd16b1
Yago Iglesias <yago.iglesias-vazquez@etu.u-paris.fr>
2023-06-26 19:07:47 +0200

    Feat: Day 8 in rust

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ pub mod day4;
 pub mod day5;
 pub mod day6;
 pub mod day7;
+pub mod day8;
 
 fn main() {
     day1::run();
@@ -14,4 +15,6 @@ fn main() {
     day5::run();
     day6::run();
     day7::run();
+    day8::run();
+
 }
